a2d1d4bd7b6427e1df8103aef14db7282eea8f4d
atetestus <atetest@californiacolleges.net>
2018-06-01 18:33:53 +0800

    start a study test script add a cts verifier_cameraformat test script

--- app/src/androidTest/java/com/lank/mytest/CtaCameraFormatTest.java
+++ app/src/androidTest/java/com/lank/mytest/CtaCameraFormatTest.java
@@ -3,12 +3,20 @@ package com.lank.mytest;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.BySelector;
 import android.support.test.uiautomator.UiDevice;
 
 import static java.lang.System.out;
 
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiScrollable;
+import android.support.test.uiautomator.UiSelector;
 import android.util.Log;
 
 import org.junit.After;
@@ -16,6 +24,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static java.lang.Thread.sleep;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
@@ -28,6 +37,9 @@ public class CtaCameraFormatTest {
     public Bundle bundle;
     public Log L;
     String TAG = "Testlog";
+    public String lens ="";
+    public String beforResolution = "";
+    public String currentResolution = "";
 
     @Before
     public void before() {
@@ -38,20 +50,95 @@ public class CtaCameraFormatTest {
         assertThat(device, notNullValue());
     }
 
-    @After
-    public void after() {
-        //L.i(TAG + "After");
-        out.println("hello world");
+    @Test
+    public void testCtsCamera() throws UiObjectNotFoundException, InterruptedException {
+        device.pressHome();
+        sleep(2000);
+        device.swipe(550,1400,550,400,5);
+        sleep(3000);
+        UiObject2 ctsApp = device.findObject(By.text("CTS Verifier"));
+        ctsApp.click();
+        device.waitForWindowUpdate("",3000);
+        Log.i(TAG, "testCtsCamera: Launch CTS Verifier");
+        try {
+            while (device.hasObject(By.text("ALLOW"))) {
+                UiObject2 press = device.findObject(By.text("ALLOW"));
+                press.click();
+                device.waitForWindowUpdate("", 3000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //測試Camera Formats
+        UiScrollable camera = new UiScrollable(new UiSelector().scrollable(true));
+        UiObject cameraFormats = camera.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()),"Camera Formats");
+        cameraFormats.click();
+        device.waitForWindowUpdate("", 3000);
+        sleep(3000);
+        try {
+            while (device.hasObject(By.text("OK"))) {
+                UiObject2 press = device.findObject(By.text("OK"));
+                press.click();
+                device.waitForWindowUpdate("", 3000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //UiObject2 camera0 = device.findObject(By.text("Camera 0"));
+        //UiObject2 camera1 = device.findObject(By.text("Camera 1"));
+
+        //開始測試Camera0
+        //176x144
+        lens = "Camera 0";
+        beforResolution = "null";
+        currentResolution = "176 x 144";
+        modeTest(lens,beforResolution,currentResolution);
+
+    }
+
+    public void modeTest(String lens,String beforResolution,String currentResolution) throws UiObjectNotFoundException {
+        this.lens = lens;
+        this.beforResolution = beforResolution;
+        this.currentResolution = currentResolution;
+
+        UiObject2 camera = device.findObject(By.text(lens));
+        camera.click();
+        sleep(3000);
+        camera.click();
+        sleep(3000);
+        if (beforResolution.equals("null")) {
+            out.println("176x144 resolution");
+        }
+        else {
+            UiObject2 Resolution1 = device.findObject(By.text(beforResolution));
+            Resolution1.click();
+        }
+        sleep(3000);
+        UiScrollable mode = new UiScrollable(new UiSelector().scrollable(true));
+        UiObject Resolution2 = mode.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()),currentResolution);
+        Resolution2.click();
+        sleep(3000);
+        UiObject2 NV21Mode = device.findObject(By.text("NV21"));
+        NV21Mode.click();
+        sleep(3000);
+        UiObject2 YV12Mode = device.findObject(By.text("YV12"));
+        YV12Mode.click();
+        sleep(3000);
+        YV12Mode.click();
+        sleep(3000);
+        NV21Mode.click();
+        sleep(3000);
     }
 
 
-
-
-
-
-
-
-
-
+    public void sleep(int mint){
+        try {
+            Thread.sleep(mint);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
