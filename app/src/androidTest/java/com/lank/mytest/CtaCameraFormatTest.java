5b310c3c96435b23cf04f6741ac3ffefffe89a48
atetestus <atetestus@gmail.com>
2018-06-04 17:40:14 +0800

    finish ctscameraformattest

--- app/src/androidTest/java/com/lank/mytest/CtaCameraFormatTest.java
+++ app/src/androidTest/java/com/lank/mytest/CtaCameraFormatTest.java
@@ -28,6 +28,9 @@ import static java.lang.Thread.sleep;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
+
+//m instrument -w -r   -e debug false -e class 'com.lank.mytest.CtaCameraFormatTest' com.lank.mytest.test/android.support.test.runner.AndroidJUnitRunner
+
 @RunWith(AndroidJUnit4.class)
 
 public class CtaCameraFormatTest {
@@ -37,7 +40,6 @@ public class CtaCameraFormatTest {
     public Bundle bundle;
     public Log L;
     String TAG = "Testlog";
-    public String lens ="";
     public String beforResolution = "";
     public String currentResolution = "";
 
@@ -86,49 +88,176 @@ public class CtaCameraFormatTest {
             e.printStackTrace();
         }
 
-        //UiObject2 camera0 = device.findObject(By.text("Camera 0"));
-        //UiObject2 camera1 = device.findObject(By.text("Camera 1"));
+        //開始測試Camera 0
+        UiObject2 len0 = device.findObject(By.text("Camera 0"));
+        len0.click();
+        sleep(2000);
+        UiObject2 camera0 = device.findObject(By.text("Camera 0"));
+        camera0.click();
+        sleep(3000);
 
-        //開始測試Camera0
         //176x144
-        lens = "Camera 0";
-        beforResolution = "null";
+        beforResolution = "176 x 144";
         currentResolution = "176 x 144";
-        modeTest(lens,beforResolution,currentResolution);
+        modeTest(beforResolution,currentResolution);
+
+        //320x240
+        beforResolution = "176 x 144";
+        currentResolution = "320 x 240";
+        modeTest(beforResolution,currentResolution);
+
+        //640x480
+        beforResolution = "320 x 240";
+        currentResolution = "640 x 480";
+        modeTest(beforResolution,currentResolution);
+
+        //960x960
+        beforResolution = "640 x 480";
+        currentResolution = "960 x 960";
+        modeTest(beforResolution,currentResolution);
+
+        //1080x1080
+        beforResolution = "960 x 960";
+        currentResolution = "1080 x 1080";
+        modeTest(beforResolution,currentResolution);
+
+        //1280x640
+        beforResolution = "1080 x 1080";
+        currentResolution = "1280 x 640";
+        modeTest(beforResolution,currentResolution);
+
+        //1280x720
+        beforResolution = "1280 x 640";
+        currentResolution = "1280 x 720";
+        modeTest(beforResolution,currentResolution);
+
+        //1280x960
+        beforResolution = "1280 x 720";
+        currentResolution = "1280 x 960";
+        modeTest(beforResolution,currentResolution);
+
+        //1680x1680
+        beforResolution = "1280 x 960";
+        currentResolution = "1680 x 1680";
+        modeTest(beforResolution,currentResolution);
+
+        //1920x960
+        beforResolution = "1680 x 1680";
+        currentResolution = "1920 x 960";
+        modeTest(beforResolution,currentResolution);
+
+        //1920x1080
+        beforResolution = "1920 x 960";
+        currentResolution = "1920 x 1080";
+        modeTest(beforResolution,currentResolution);
+
+        //1920x1440
+        beforResolution = "1920 x 1080";
+        currentResolution = "1920 x 1440";
+        modeTest(beforResolution,currentResolution);
+
+        //開始測試Camera 1
+        UiObject2 len1 = device.findObject(By.text("Camera 0"));
+        len1.click();
+        sleep(2000);
+        UiObject2 camera1 = device.findObject(By.text("Camera 1"));
+        camera1.click();
+        sleep(3000);
+
+        //176x144
+        beforResolution = "176 x 144";
+        currentResolution = "176 x 144";
+        modeTest(beforResolution,currentResolution);
+
+        //320x240
+        beforResolution = "176 x 144";
+        currentResolution = "320 x 240";
+        modeTest(beforResolution,currentResolution);
+
+        //640x480
+        beforResolution = "320 x 240";
+        currentResolution = "640 x 480";
+        modeTest(beforResolution,currentResolution);
+
+        //960x960
+        beforResolution = "640 x 480";
+        currentResolution = "960 x 960";
+        modeTest(beforResolution,currentResolution);
+
+        //1080x1080
+        beforResolution = "960 x 960";
+        currentResolution = "1080 x 1080";
+        modeTest(beforResolution,currentResolution);
+
+        //1280x640
+        beforResolution = "1080 x 1080";
+        currentResolution = "1280 x 640";
+        modeTest(beforResolution,currentResolution);
+
+        //1280x720
+        beforResolution = "1280 x 640";
+        currentResolution = "1280 x 720";
+        modeTest(beforResolution,currentResolution);
+
+        //1280x960
+        beforResolution = "1280 x 720";
+        currentResolution = "1280 x 960";
+        modeTest(beforResolution,currentResolution);
+
+        //1680x1680
+        beforResolution = "1280 x 960";
+        currentResolution = "1680 x 1680";
+        modeTest(beforResolution,currentResolution);
+
+        //1920x960
+        beforResolution = "1680 x 1680";
+        currentResolution = "1920 x 960";
+        modeTest(beforResolution,currentResolution);
+
+        //1920x1080
+        beforResolution = "1920 x 960";
+        currentResolution = "1920 x 1080";
+        modeTest(beforResolution,currentResolution);
+
+        //1920x1440
+        beforResolution = "1920 x 1080";
+        currentResolution = "1920 x 1440";
+        modeTest(beforResolution,currentResolution);
+
+        //TestFinish
+        UiObject2 pass = device.findObject(By.res("com.android.cts.verifier:id/pass_button"));
+        pass.click();
+        sleep(2000);
+
+        device.pressBack();
+        device.pressBack();
+        device.pressBack();
 
     }
 
-    public void modeTest(String lens,String beforResolution,String currentResolution) throws UiObjectNotFoundException {
-        this.lens = lens;
+    public void modeTest(String beforResolution,String currentResolution) throws UiObjectNotFoundException {
+
         this.beforResolution = beforResolution;
         this.currentResolution = currentResolution;
 
-        UiObject2 camera = device.findObject(By.text(lens));
-        camera.click();
-        sleep(3000);
-        camera.click();
-        sleep(3000);
-        if (beforResolution.equals("null")) {
-            out.println("176x144 resolution");
-        }
-        else {
-            UiObject2 Resolution1 = device.findObject(By.text(beforResolution));
-            Resolution1.click();
-        }
-        sleep(3000);
-        UiScrollable mode = new UiScrollable(new UiSelector().scrollable(true));
-        UiObject Resolution2 = mode.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()),currentResolution);
+        UiObject2 Resolution1 = device.findObject(By.text(beforResolution));
+        Resolution1.click();
+        sleep(2000);
+        UiScrollable modeSelect = new UiScrollable(new UiSelector().scrollable(true));
+        UiObject Resolution2 = modeSelect.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()),currentResolution);
         Resolution2.click();
+        sleep(2000);
+        UiObject2 NV21Mode1 = device.findObject(By.text("NV21"));
+        NV21Mode1.click();
+        sleep(2000);
+        UiObject2 YV12Mode1 = device.findObject(By.text("YV12"));
+        YV12Mode1.click();
         sleep(3000);
-        UiObject2 NV21Mode = device.findObject(By.text("NV21"));
-        NV21Mode.click();
-        sleep(3000);
-        UiObject2 YV12Mode = device.findObject(By.text("YV12"));
-        YV12Mode.click();
-        sleep(3000);
-        YV12Mode.click();
-        sleep(3000);
-        NV21Mode.click();
+        UiObject2 YV12Mode2 = device.findObject(By.text("YV12"));
+        YV12Mode2.click();
+        sleep(2000);
+        UiObject2 NV21Mode2 = device.findObject(By.text("NV21"));
+        NV21Mode2.click();
         sleep(3000);
     }
 
--- app/src/main/java/com/lank/mytest/MainActivity.java
+++ app/src/main/java/com/lank/mytest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lank.mytest;
 
+import android.app.UiAutomation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void runCtsCameraCameraFormats(View v){
+        new UiAutomatorThread().start();
+    }
+
+    class UiAutomatorThread extends Thread{
+        @Override
+        public void run(){
+            super.run();
+            String command = generateCommand("com.lank.mytest","CtaCameraFormatTest");
+            try {
+                Process p = Runtime.getRuntime().exec(command);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+
+        }
+//m instrument -w -r   -e debug false -e class 'com.lank.mytest.CtaCameraFormatTest' com.lank.mytest.test/android.support.test.runner.AndroidJUnitRunner
+        public String generateCommand(String pkgName,String clsName){
+            String command = "am instrument --user 0 -w -r   -e debug false -e class "+pkgName+"."+clsName+" "+pkgName+".test/android.support.test.runner.AndroidJUnitRunner";
+            return command;
+        }
+
+    }
+
 }
